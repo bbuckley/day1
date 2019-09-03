492ba275d2106bb51b501fc0e6d4facb242f0331
brian buckley <briankbuckley@gmail.com>
2019-09-03 14:16:24 -0400

    discard noop

--- src/Main.elm
+++ src/Main.elm
@@ -8,16 +8,10 @@ n =
     11
 
 
-type Msg
-    = Inc
-    | Dec
-
-
 main : Html msg
 main =
     div []
         [ p [] [ "n is " ++ String.fromInt n |> text ]
         , p [] [ String.concat (List.repeat n "x") |> text ]
         , p [] [ List.repeat n "." |> String.concat |> text ]
-        , p [] [ button [] [ text "-" ], button [] [ text "+" ] ]
         ]
