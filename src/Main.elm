017392f46d0b128a4615a2b6e603db23b045a4e9
brian buckley <briankbuckley@gmail.com>
2019-09-03 14:11:40 -0400

    add buttons

--- src/Main.elm
+++ src/Main.elm
@@ -1,11 +1,16 @@
 module Main exposing (main)
 
-import Html exposing (Html, div, p, text)
+import Html exposing (Html, button, div, p, text)
 
 
 n : Int
 n =
     11
+
+
+type Msg
+    = Inc
+    | Dec
 
 
 main : Html msg
@@ -14,4 +19,5 @@ main =
         [ p [] [ "n is " ++ String.fromInt n |> text ]
         , p [] [ String.concat (List.repeat n "x") |> text ]
         , p [] [ List.repeat n "." |> String.concat |> text ]
+        , p [] [ button [] [ text "-" ], button [] [ text "+" ] ]
         ]
